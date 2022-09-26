2306639d2b49ec81713f5f5cf89ac6a37283ddad
Rishabh Jain <rishabh.jain@corevaluetech.com>
2022-09-27 00:26:24 +0530

    Added navbar basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
 import './App.scss';
+import Navbar from './components/NavBar';
+
+import { ReactComponent as HomeIcon } from './assets/icons/homeIcon.svg';
+import { ReactComponent as TvIcon } from './assets/icons/tvIcon.svg';
+import { ReactComponent as CompanyLogo } from './assets/logos/logoDisneyPlusOnly.svg';
+import { ReactComponent as CinemaIcon } from './assets/icons/cinemaIcon.svg';
+import { ReactComponent as SportsIcon } from './assets/icons/sportIcon.svg';
+import { BrowserRouter } from 'react-router-dom';
+
+const navlinks = [
+  {
+    icon: <HomeIcon />,
+    title: 'Home',
+    linkTo: '/home',
+  },
+  {
+    icon: <TvIcon stroke="var(--TEXT_COLOR_L4)" />,
+    title: 'TV',
+    linkTo: '/tv',
+  },
+  {
+    icon: <CompanyLogo />,
+    linkTo: '/home',
+  },
+  {
+    icon: <CinemaIcon />,
+    title: 'Movies',
+    linkTo: '/home',
+  },
+  {
+    icon: <SportsIcon />,
+    title: 'Sports',
+    linkTo: '/sports',
+  },
+];
 
 function App() {
-  return <div>Hotstar Clone</div>;
+  return (
+    <BrowserRouter>
+      <Navbar navlinks={navlinks} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/IconWithTitleLink/index.tsx
+++ src/components/IconWithTitleLink/index.tsx
@@ -2,17 +2,21 @@ import { FC } from 'react';
 import { Link, Path } from 'react-router-dom';
 import './style.scss';
 
-interface IIconWithTitleLink {
+export interface IIconWithTitleLink {
   icon: JSX.Element;
-  title: string;
+  title?: string;
   linkTo: string | Partial<Path>;
 }
 
-const IconWithTitleLink: FC<IIconWithTitleLink> = ({ icon, title, linkTo }) => {
+const IconWithTitleLink: FC<IIconWithTitleLink> = ({
+  icon,
+  title = '',
+  linkTo,
+}) => {
   return (
     <Link to={linkTo} className="IconWithTitleLink">
-      <div className="IconWithTitleLink__icon">{icon}</div>
-      <div className="IconWithTitleLink__title">{title}</div>
+      {icon}
+      {title && <div className="IconWithTitleLink__title">{title}</div>}
     </Link>
   );
 };
