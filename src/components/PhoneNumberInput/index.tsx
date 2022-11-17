e298567eb8b89b446f4a5fe17611123e9ea4537f
Rishabh2324 <rishabh2401jain@gmail.com>
2022-11-17 18:02:42 +0530

    Added useReducer to login page and replace useState approach

--- src/components/PhoneNumberInput/index.tsx
+++ src/components/PhoneNumberInput/index.tsx
@@ -1,37 +1,23 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import PhoneInput from 'react-phone-input-2';
-
-import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 import { ReactComponent as WarningIcon } from '../../assets/icons/warningIcon.svg';
 
 import './style.scss';
-
 interface IInput {
   value: string | undefined;
+  errorMessage?: string;
   onChange: Function;
   className?: string;
 }
 
-const PhoneNumberInput: FC<IInput> = ({ value, onChange, className = '' }) => {
-  const ref = useRef(null);
+const PhoneNumberInput: FC<IInput> = ({
+  value,
+  onChange,
+  errorMessage,
+  className = '',
+}) => {
   const [data, setData] = useState<IInput['value']>();
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const withOutDialCode = (data: string) => data?.slice(2);
-
-  const isValidPhoneNumber = (data: string) =>
-    withOutDialCode(data).length === 10;
-
-  useOnClickOutside(ref, () => {
-    setErrorMessage(
-      value && withOutDialCode(value).length > 0
-        ? isValidPhoneNumber(value)
-          ? ''
-          : 'Invalid phone number'
-        : 'Please enter your mobile number'
-    );
-  });
 
   const handleChange = (value: string) => {
     setData(value);
@@ -43,7 +29,7 @@ const PhoneNumberInput: FC<IInput> = ({ value, onChange, className = '' }) => {
   }, [value]);
 
   return (
-    <div ref={ref}>
+    <>
       <PhoneInput
         specialLabel=""
         country="in"
@@ -62,7 +48,7 @@ const PhoneNumberInput: FC<IInput> = ({ value, onChange, className = '' }) => {
           <WarningIcon className="PhoneNumberInput__error___icon" />
         </div>
       ) : null}
-    </div>
+    </>
   );
 };
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useReducer, useRef } from 'react';
+
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,83 +11,151 @@ import { ReactComponent as BackIcon } from '../../assets/icons/backIcon.svg';
 
 import './style.scss';
 
-type LoginType = 'email' | 'phoneNumber';
+const initialState = {
+  loginType: 'phone',
+  phone: {
+    value: '',
+    errorMessage: '',
+    totalCharacters: 0, // length with out dial code
+    isValid: null,
+  },
+  email: {
+    value: '',
+    errorMessage: '',
+    isValid: null,
+  },
+};
+
+const reducer = (state: typeof initialState, action: typeof initialState) => {
+  return {
+    ...state,
+    ...action,
+  };
+};
 
 const Login = () => {
-  const [loginType, setLoginType] = useState<LoginType>('phoneNumber');
-  const [phoneNumber, setPhoneNumber] = useState({ value: '', isValid: true });
-  const [email, setEmail] = useState('');
-  const [showContinueCta, setShowContinueCta] = useState(true);
+  const ref = useRef(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { loginType, email, phone } = state;
 
-  const handleChange = (value: string) => {
-    setPhoneNumber({
-      ...phoneNumber,
-      value,
-    });
+  const handleChange = (
+    key: string,
+    value: string,
+    parentKey: 'email' | 'phone' | null,
+    isBulkUpdate?: boolean,
+    bulkData?: typeof initialState
+  ) => {
+    if (isBulkUpdate) {
+      // for bulk update or simple scalar values
+      dispatch({
+        ...state,
+        ...bulkData,
+      });
+    } else if (key && parentKey) {
+      // for nested objects
+      dispatch({
+        ...state,
+        [parentKey]: {
+          ...state[parentKey],
+          [key]: value,
+        },
+      });
+    }
   };
 
-  const handleEmailChange = (value: string) => {
-    setEmail(value);
-  };
+  const phoneWithOutDialCode = (phnNumber: string) => phnNumber.slice(2);
 
-  useEffect(() => {
-    setShowContinueCta(
-      (phoneNumber && phoneNumber.value?.slice(2).length > 0) ||
-        email.length > 0
-    );
-  }, [phoneNumber, email]);
+  const isValidPhoneNumber = (data: string) => data.length === 10;
+
+  useOnClickOutside(ref, () => {
+    const phnNumber = phoneWithOutDialCode(phone.value);
+    const updatedValues: [string, string, 'phone' | 'email' | null] = phnNumber
+      ? isValidPhoneNumber(phnNumber)
+        ? ['errorMessage', '', 'phone']
+        : ['errorMessage', 'Invalid phone number', 'phone']
+      : ['errorMessage', 'Please enter your mobile number', 'phone'];
+    handleChange(...updatedValues);
+  });
 
   return (
     <div className="Login">
-      {loginType === 'phoneNumber' ? (
+      {loginType === 'phone' ? (
         <>
-          {showContinueCta && (
+          {phone.totalCharacters > 0 && (
             <BackIcon
               className="Login__backIcon"
-              onClick={() => {
-                setPhoneNumber({ ...phoneNumber, value: '91' });
-              }}
+              onClick={() =>
+                handleChange('', '', null, true, {
+                  ...state,
+                  phone: {
+                    ...phone,
+                    value: '91', // init with dialcode
+                    totalCharacters: 0,
+                  },
+                })
+              }
             />
           )}
           <h5 className="Login__heading">
-            {showContinueCta ? 'Continue using phone' : 'Login to continue'}
+            {phone.totalCharacters > 0
+              ? 'Continue using phone'
+              : 'Login to continue'}
           </h5>
-          {!showContinueCta ? (
+          {phone.totalCharacters === 0 ? (
             <>
               <OutlinedButton
                 className="Login__cta"
                 buttonText="Have a Email Account?"
-                onClick={() => setLoginType('email')}
+                onClick={() =>
+                  handleChange('', '', null, true, {
+                    ...initialState,
+                    loginType: 'email',
+                  })
+                }
               />
               <p className="Login__subText">or</p>
             </>
           ) : null}
-          <PhoneNumberInput
-            value={phoneNumber?.value}
-            onChange={handleChange}
-            className="Login__input"
-          />
+          <div ref={ref}>
+            <PhoneNumberInput
+              value={phone.value}
+              errorMessage={phone.errorMessage}
+              onChange={(value: string) =>
+                handleChange('', '', null, true, {
+                  ...state,
+                  phone: {
+                    ...phone,
+                    value,
+                    totalCharacters: phoneWithOutDialCode(value).length,
+                  },
+                })
+              }
+              className="Login__input"
+            />
+          </div>
         </>
       ) : (
         <>
           <BackIcon
             className="Login__backIcon"
-            onClick={() => {
-              setLoginType('phoneNumber');
-              handleEmailChange('');
-            }}
+            onClick={() =>
+              handleChange('', '', null, true, {
+                ...initialState,
+                loginType: 'phone',
+              })
+            }
           />
           <h5 className="Login__heading">Enter your email</h5>
           <Input
-            value={email}
-            onChange={handleEmailChange}
+            value={email.value}
+            onChange={(value: string) => handleChange('value', value, 'email')}
             className="Login__input"
             placeHolder="Enter your email"
             autoFocus
           />
         </>
       )}
-      {showContinueCta ? (
+      {email.value || phone.totalCharacters ? (
         <Button
           className="Login__cta"
           buttonText="Continue to login"
